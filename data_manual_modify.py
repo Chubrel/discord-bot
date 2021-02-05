963338266fa2adb097dfd4942bcf1e09029cccb5
Chubrel <maxim.potapov2002@gmail.com>
2021-02-06 01:31:54 +0300

    bot update

--- data_manual_modify.py
+++ data_manual_modify.py
@@ -30,6 +30,50 @@ def get_some_data():
             print(2, k.tests.ench.__dict__)
 
 
+def try_loop(arg):
+    try:
+        for _ in arg:
+            return True
+    except TypeError:
+        return False
+
+
+def try_dict(arg):
+    try:
+        t = arg.__dict__
+    except AttributeError:
+        return None
+    else:
+        return t
+
+
+objs = []
+import _asyncio
+
+
+def values(arg):
+    global objs
+    if arg is None or arg in objs:
+        return
+    else:
+        objs.append(arg)
+        if isinstance(arg, _asyncio.Task):
+            print('lol <<<<<<<<<<<<<<<<<<<<<<<<<<')
+    t = try_dict(arg)
+    if t is not None:
+        values(t)
+    elif isinstance(arg, dict):
+        for k, v in arg.items():
+            if not isinstance(k, str) or not k.startswith('__'):
+                values(v)
+    else:
+        if try_loop(arg):
+            for i in arg:
+                values(i)
+        else:
+            print(arg)
+
+
 # Заканчиваем штуки
 def save():
     global data
